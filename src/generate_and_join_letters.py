95631ad901d374fd60fb7a6da3c3efb3778751b5
Lenaosama <60302049@udst.edu.qa>
2025-02-25 15:20:03 +0300

    Delete src/generate_and_join_letters.py

--- src/generate_and_join_letters.py
+++
@@ -1,7 +0,0 @@
-
-def generate_and_join_letters(n: int = 1000):
-    letters = ''
-    for i in range(n):
-        letters += chr(random.randint(33, 126))
-        # letters += random.choice(string.ascii_letters + string.digits)
-    return letters
